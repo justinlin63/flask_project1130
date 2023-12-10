674a8def066feb9616cbcb12ffad7de1b59f7ab5
justinlin63 <lienlin63@gmail.com>
2023-12-10 23:02:09 +0800

    add

--- app/config.py
+++ app/config.py
@@ -8,10 +8,8 @@ class Configs:
     password = 'admin'
 
 
-# ip = '10.70.5.128'
 ip = '192.168.1.110'
 fp_ip = '192.168.1.116'
-# ip = '172.20.10.2'
 host = "0.0.0.0"
 port = 80
 path = str(Path.cwd()) + r"\app" + "\\"
