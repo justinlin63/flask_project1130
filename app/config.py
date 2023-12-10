0d852b6ac273c81c39dfa0d808769d29d80d23de
justinlin63 <lienlin63@gmail.com>
2023-12-10 23:02:51 +0800

    add

--- app/config.py
+++ app/config.py
@@ -8,11 +8,12 @@ class Configs:
     password = 'admin'
 
 
-ip = '192.168.1.110'
 fp_ip = '192.168.1.116'
+uid = 'justin_shop'
+key = '1172'
+
+ip = '192.168.1.110'
 host = "0.0.0.0"
 port = 80
 path = str(Path.cwd()) + r"\app" + "\\"
 limit_per_page = 5
-uid = 'justin_shop'
-key = '1172'
