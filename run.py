7cd3d01569f0d941137a21d1a708b010d4b4a8bd
justinlin63 <lienlin63@gmail.com>
2023-12-13 20:11:12 +0800

    fix

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import create_app, host, port
 import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host=host, port=port)
+    app.run()
